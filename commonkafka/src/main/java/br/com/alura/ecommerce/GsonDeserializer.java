ecac3a7e1864ae603824693c509dae20412764da
Jonatas Carvalho <jonatas@Jojo.local>
2022-08-20 18:27:51 -0300

    adding entire project about an ecommerce using java and kafka

---
+++ commonkafka/src/main/java/br/com/alura/ecommerce/GsonDeserializer.java
@@ -0,0 +1,31 @@
+package br.com.alura.ecommerce;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.util.Map;
+
+import static java.lang.Class.forName;
+
+public class GsonDeserializer<T> implements Deserializer {
+
+    public static final String TYPE_CONFIG = "br.com.alura.ecommerce.type_config";
+    private final Gson gson = new GsonBuilder().create();
+    private Class<T> type;
+
+    @Override
+    public void configure(Map configs, boolean isKey) {
+        String typeName = String.valueOf(configs.get(TYPE_CONFIG));
+        try {
+            this.type = (Class<T>) Class.forName(typeName);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Type for deserialization does not exist in the classpath");
+        }
+    }
+
+    @Override
+    public T deserialize(String s, byte[] bytes) {
+        return gson.fromJson(new String(bytes), type);
+    }
+}
